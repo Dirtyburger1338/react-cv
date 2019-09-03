c9d57433bcc0e3778b220e8eca6587205d57e03b
Nilsson <per.a.nilsson@sogeti.com>
2019-09-03 11:20:41 +0200

    Updates

---
+++ precache-manifest.b728a856ac65a81dac9b2e221c9e4461.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f790ac7bd80836e77370e511908d367",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "b947ae043d7c5dd57af7",
+    "url": "/react-cv/static/css/main.f33314cf.chunk.css"
+  },
+  {
+    "revision": "2fd78d1acf2b8b2ba50f",
+    "url": "/react-cv/static/js/2.43afca0d.chunk.js"
+  },
+  {
+    "revision": "b947ae043d7c5dd57af7",
+    "url": "/react-cv/static/js/main.95cfa09b.chunk.js"
+  },
+  {
+    "revision": "d8f97377dad5c9d315fe",
+    "url": "/react-cv/static/js/runtime~main.59df248e.js"
+  },
+  {
+    "revision": "86f49ad5376894172cfcfa8155f88ba3",
+    "url": "/react-cv/static/media/Chrome-icon.86f49ad5.png"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/react-cv/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "dedf478dc323874baa7eaf90492ce5b0",
+    "url": "/react-cv/static/media/cmd-icon.dedf478d.ico"
+  },
+  {
+    "revision": "f81d3bf5bb4e4d45eeb45c6e81b1c703",
+    "url": "/react-cv/static/media/imageres_5367.f81d3bf5.ico"
+  },
+  {
+    "revision": "f80596190df6735d3db97bc4410d6338",
+    "url": "/react-cv/static/media/more-settings.f8059619.svg"
+  },
+  {
+    "revision": "b98de4539f9ca54e380b77912ee41d2d",
+    "url": "/react-cv/static/media/note-icon.b98de453.ico"
+  },
+  {
+    "revision": "bdc4ab6b31161aaa77d6381539c4cc73",
+    "url": "/react-cv/static/media/shell32_264.bdc4ab6b.ico"
+  },
+  {
+    "revision": "3a08388af7d5b80894d4f4ef1d888913",
+    "url": "/react-cv/static/media/star-favourite.3a08388a.png"
+  },
+  {
+    "revision": "ed75434451a06db954bba28e359802b6",
+    "url": "/react-cv/static/media/win-bg.ed754344.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.455c0865bd9fb03d357fa27007c8d940.js"
+  "/react-cv/precache-manifest.b728a856ac65a81dac9b2e221c9e4461.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
